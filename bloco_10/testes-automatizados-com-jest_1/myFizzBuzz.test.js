416cfe04fa21e267f3dddc58d37b508044982bdb
Lets <letsferreiratx@gmail.com>
2020-10-30 19:59:28 -0300

    done

---
+++ bloco_10/testes-automatizados-com-jest_1/myFizzBuzz.test.js
@@ -0,0 +1,18 @@
+
+const myFizzBuzz = require('./myFizzBuzz');
+
+test('if it returns fizzbuzz', () => {
+  expect(myFizzBuzz(15)).toBe('fizzbuzz');
+});
+
+test('if it returns fizz', () => {
+  expect(myFizzBuzz(9)).toBe('fizz');
+});
+
+test('if it returns buzz', () => {
+  expect(myFizzBuzz(20)).toBe('buzz');
+});
+
+test('if receves only numbers', () => {
+  expect(myFizzBuzz('string')).toBeFalsy();
+});
---
+++ bloco_10/testes-automatizados-com-jest_1/myRemove.test.js
@@ -0,0 +1,20 @@
+
+const myRemove = require('./myRemove');
+
+
+test('if returns the expected array', () =>{
+  expect(myRemove([1, 2, 3, 4,], 3)).toEqual([1, 2, 4]);
+  expect(myRemove([1,2,3,4],5)).toEqual([1,2,3,4]);
+});
+
+test('if it do not return a wrong array', () =>{
+  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1 ,2 ,3 ,4]);
+});
+
+test('does not change the passad array', () => {
+  const arr = [1, 2, 3, 4];
+  myRemove(arr, 1);
+  expect(arr).toEqual([1, 2, 3, 4]);
+});
+
+
